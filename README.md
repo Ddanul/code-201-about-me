f783cb5e93a59c93ab6dd63eb51025c7f81a0f92
Danul De Leon <danul.deleon@gmail.com>
2018-08-03 13:42:06 -0700

    submission to fix linter errors and readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # code-201-about-me
 This is an 'About Me' page started in lab 02
 Initially, there are five random questions asked in prompts to gauge the user's knowledge of me.  A response will be given after each answer to see if the user answered the question correctly.
+
+This version includes paired programming.
+Nic Cook was the driver.
+Danul De Leon was the navigator.
--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-"use strict";
+'use strict';
 
 console.log('js is linked');
 var score = 0;
-var answer = [];
 var randNum = Math.floor((Math.random() * 10) + 1);
 var compare = [1, 1, 0, 1, 1, randNum, ['HOUSTON', 'AUSTIN', 'CONROE']];
 var questions = [
@@ -23,7 +22,6 @@ alert('Hey there, '+ userName +'!  Let\'s play a little game.');
 //Runs through each question
 for(var i=0; i<questions.length; i++){
   var newAnswer= prompt(questions[i]).toUpperCase();
-  console.log('The user input is: '+answer[i]);
   if(i < 5){
     //Checks if answer matches yes
     if(newAnswer === 'YES' || newAnswer === 'Y'){
@@ -44,7 +42,7 @@ for(var i=0; i<questions.length; i++){
         alert('Nope!');
         console.log('The current score is: ' +score);
       }
-    }else{  //Responds if answer is not yes or no
+    }else{ //Responds if answer is not yes or no
       alert('That wasn\'t a proper answer.  Maybe check your spelling!');
     }
   }
@@ -55,7 +53,7 @@ for(var i=0; i<questions.length; i++){
       if(x>0 && x<4){
         newAnswer= prompt(questions[i]).toUpperCase();
       }
-      if(newAnswer == compare[i]){
+      if(parseInt(newAnswer) === compare[i]){
         alert('You are correct!');
         score++;
         console.log('The current score is: ' +score);
@@ -70,7 +68,7 @@ for(var i=0; i<questions.length; i++){
   }
   else{
     console.log('Cities in Texas: '+compare[i]);
-    for(var y = 0; y<6 ; y++){  // allows for 6 tries
+    for(var y = 0; y<6 ; y++){ // allows for 6 tries
       if(y > 0){
         newAnswer= prompt(questions[i]).toUpperCase();
       }
